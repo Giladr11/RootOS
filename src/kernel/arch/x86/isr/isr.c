5eba21e4eda5c1afc8e4806b5ea8999062abdbd7
giladr11 <gilad.rafael2007@gmail.com>
2024-11-20 14:25:01 +0200

    fixed isr

--- src/kernel/arch/x86/isr/isr.c
+++ src/kernel/arch/x86/isr/isr.c
@@ -32,29 +32,41 @@ void __attribute__((cdecl)) ISR_Handler(Registers* regs)
         }   
     }
 
-    else {
-        char string2[20] = "Unhandled Exception!";
+    else if (regs->interrupt == 2) {
+        char string1[22] = "Unhandled Interrupt 2!";
 
         int pos = 0;
-        while(pos < 20)
+        while(pos < 22)
         {
-            *((char*)0xb8000 + pos * 2) = string2[pos];        
+            *((char*)0xb8000 + pos * 2) = string1[pos];        
             *((char*)0xb8000 + pos * 2 + 1) = 0x07;    
             pos ++; 
         }   
-
-        // printf("Unhandled exception %d %s\n", regs->interrupt, g_Exceptions[regs->interrupt]);
-        
-        // printf("  eax=%x  ebx=%x  ecx=%x  edx=%x  esi=%x  edi=%x\n",
-        //        regs->eax, regs->ebx, regs->ecx, regs->edx, regs->esi, regs->edi);
-
-        // printf("  esp=%x  ebp=%x  eip=%x  eflags=%x  cs=%x  ds=%x  ss=%x\n",
-        //        regs->esp, regs->ebp, regs->eip, regs->eflags, regs->cs, regs->ds, regs->ss);
-
-        // printf("  interrupt=%x  errorcode=%x\n", regs->interrupt, regs->error);
-
-        // printf("KERNEL PANIC!\n");
-        CLEAR_INT_FLAG();
     }
+
+    // else {
+        // char string2[20] = "Unhandled Exception!";
+
+        // int pos = 0;
+        // while(pos < 20)
+        // {
+        //     *((char*)0xb8000 + pos * 2) = string2[pos];        
+        //     *((char*)0xb8000 + pos * 2 + 1) = 0x07;    
+        //     pos ++; 
+        // }   
+
+        // // printf("Unhandled exception %d %s\n", regs->interrupt, g_Exceptions[regs->interrupt]);
+        
+        // // printf("  eax=%x  ebx=%x  ecx=%x  edx=%x  esi=%x  edi=%x\n",
+        // //        regs->eax, regs->ebx, regs->ecx, regs->edx, regs->esi, regs->edi);
+
+        // // printf("  esp=%x  ebp=%x  eip=%x  eflags=%x  cs=%x  ds=%x  ss=%x\n",
+        // //        regs->esp, regs->ebp, regs->eip, regs->eflags, regs->cs, regs->ds, regs->ss);
+
+        // // printf("  interrupt=%x  errorcode=%x\n", regs->interrupt, regs->error);
+
+        // // printf("KERNEL PANIC!\n");
+        // CLEAR_INT_FLAG();
+    //}
 }
 
--- src/kernel/arch/x86/isr/isr_handler.asm
+++ src/kernel/arch/x86/isr/isr_handler.asm
@@ -6,7 +6,7 @@ extern ISR_Handler
 
 %macro ISR_NOERROR_CODE 1
 
-global ISR%1
+global ISR%1:
 ISR%1:
     push 0              ; push dummy error code
     push %1             ; push interrupt number
@@ -15,7 +15,8 @@ ISR%1:
 %endmacro 
 
 %macro ISR_ERROR_CODE 1
-global ISR%1
+
+global ISR%1:
 ISR%1:
     push %1
     jmp isr_common
@@ -33,23 +34,23 @@ isr_common:
 
     mov ax, 0x10        ; gdt ds
     mov ds, ax  
-    mov es, ax  
-    mov gs, ax  
+    mov es, ax   
     mov fs, ax  
+    mov gs, ax 
 
     push esp            ; pass pointer to stack to C, so we can access all the pushed info
 
     call ISR_Handler
 
-    add esp, 0x4
+    add esp, 4
 
     pop eax             ; restore old segment
     mov ds, ax
     mov es, ax
-    mov gs, ax
     mov fs, ax
+    mov gs, ax
 
     popa                ; pop general purpose regs
-    add esp, 0x8        ; remove error code and interrupt number
+    add esp, 8        ; remove error code and interrupt number
 
     iret                ; return from isr and pop: (ss, esp, eflags, cs, eip)
